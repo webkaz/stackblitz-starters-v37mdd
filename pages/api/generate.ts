6e3bd0303d12950073ea020754fd153af42afeab
webkaz <web.kaz@gmail.com>
2023-08-17 11:04:28 +0000

    add fonts

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -5,7 +5,9 @@ import path from "path";
 import sharp from "sharp";
 import texttosvg from "text-to-svg";
 
-let fontSize = 30;
+const imageDir = path.join(process.cwd(), "images");
+const fontDir = path.join(process.cwd(), "fonts");
+let fontSize = 20;
 const svgOptions = {
   x: 0,
   y: 0,
@@ -13,7 +15,7 @@ const svgOptions = {
   anchor: "left  top",
   attributes: { fill: "white", stroke: "black" },
 };
-const text2svg = texttosvg.loadSync();
+const text2svg = texttosvg.loadSync(fontDir + "/ipaexm.ttf");
 
 /**
  * 文字列をSVGパスに変換
@@ -34,35 +36,34 @@ async function genSvgStr(str: string) {
 }
 
 export default async (req, res) => {
-  const imageDir = path.join(process.cwd(), "images");
   const img_in_path: string = imageDir + "/pooh.avif";
   const img_out_path: string = imageDir + "/pooh_result.avif";
-  const outputText = '© Disney. Based on the "Winnie the Poohdddd"';
+  const outputText = '© Disney. Based on the "Winnie the Pooh"';
   const outputText2 = "works by A.A. Milne and E.H. Shepard.";
   //元画像の読み込み
   const img = await sharp(img_in_path);
   const height = (await img.metadata()).height;
   const width = (await img.metadata()).width;
 
+  //画像にSVGを合成する
   await img
     .composite([
       {
         input: Buffer.from(await text2svg.getSVG(outputText, svgOptions)),
         top: height - fontSize * 2,
-        left: width - (await text2svg.getMetrics(outputText, svgOptions).width),
+        left:
+          width -
+          Math.ceil(await text2svg.getMetrics(outputText, svgOptions).width),
       },
       {
         input: Buffer.from(await text2svg.getSVG(outputText2, svgOptions)),
         top: height - fontSize,
         left:
-          width - (await text2svg.getMetrics(outputText2, svgOptions).width),
+          width -
+          Math.ceil(await text2svg.getMetrics(outputText2, svgOptions).width),
       },
     ])
     .toFile(img_out_path);
-
-  //画像を反転させてコピー
-  //const resultImg = await img.clone().flip().toFile(img_out_path);
-  // await img.clone().flop().toFile(img_out_path);
 
   //生成したファイルのパスを返す
   res.status(200).json({ resultPath: "" });
